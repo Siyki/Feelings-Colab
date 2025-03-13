b877184ebe48ad2cfef8f6901671eaf86b87493f
Siyki <isabeta.mar@gmail.com>
2025-03-13 10:06:22 -0500

    Update app.py

--- tx2_analisis-main/tx2_analisis-main/app.py
+++ tx2_analisis-main/tx2_analisis-main/app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 from textblob import TextBlob
 from googletrans import Translator
+from streamlit-lottie import st_lottie
+import json
 
 translator = Translator()
 st.title('Uso de textblob')
